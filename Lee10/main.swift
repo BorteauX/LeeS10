818aba2dbac993ebfe59a2c80f4c81a2c3ff46a8
user <user@userde-MacBook-8.local>
2017-06-16 14:35:18 +0800

    20170616

--- Lee10/main.swift
+++ Lee10/main.swift
@@ -5,8 +5,16 @@
 //  Created by user on 2017/6/16.
 //  Copyright © 2017年 user. All rights reserved.
 //
-
+//9*9
 import Foundation
 
+var obj1 = MyClass01(x:2)
+obj1.x = 8
+for i in 1...9 {
+    print(obj1[i])
+}
+print(obj1[3,4])
 
-
+print(obj1[3.14])
+obj1[3.14] = 7.456
+print(obj1.x)
